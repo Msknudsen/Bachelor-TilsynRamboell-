b9b5a02ef11c40d5a9d10fde23acabe1d0097a7e
Ao Li <liao@live.dk>
2017-09-27 14:48:10 +0200

    project files clean up

--- Kode/Ramboell/Ramboell/Ramboell/MainPage.xaml.cs
+++ Kode/Ramboell/Ramboell/Ramboell/MainPage.xaml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Ramboell
 {
-	public partial class MainPage : ContentPage
+
+    public partial class MainPage : ContentPage
 	{
 
 		public MainPage()
